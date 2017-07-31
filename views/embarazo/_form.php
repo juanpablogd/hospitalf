e595e2279b23addef231139d07952bd023985917
Juan Pablo GD <juanpablogarzond@gmail.com>
2017-07-31 16:05:53 -0500

    Cambio _form

--- views/embarazo/_form.php
+++ views/embarazo/_form.php
@@ -31,7 +31,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'id_gt_p_estado')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? 'Crear' : 'Actualizar', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
