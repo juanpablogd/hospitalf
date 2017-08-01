b3c4edc442ba31a23d5edde158ec1a7aa9065c0b
Juan Pablo GD <juanpablogarzond@gmail.com>
2017-07-31 19:03:53 -0500

    Modf. view Estado

--- views/estado/view.php
+++ views/estado/view.php
@@ -6,7 +6,7 @@ use yii\widgets\DetailView;
 /* @var $this yii\web\View */
 /* @var $model app\models\pestado */
 
-$this->title = $model->id;
+$this->title = $model->estado;
 $this->params['breadcrumbs'][] = ['label' => 'Estados', 'url' => ['index']];
 $this->params['breadcrumbs'][] = $this->title;
 ?>
@@ -28,7 +28,6 @@ $this->params['breadcrumbs'][] = $this->title;
     <?= DetailView::widget([
         'model' => $model,
         'attributes' => [
-            'id',
             'estado',
             'detalle_estado',
             'min_semana',
