764744f05443ca859025f741a5463239f9242ef4
Juan Pablo GD <juanpablogarzond@gmail.com>
2017-08-10 17:31:08 -0500

    Revert "Pié de página - Realizado x"
    This reverts commit 8eea3f069dfdc56403e177e48d11f36f4b25104b.

--- views/layouts/main.php
+++ views/layouts/main.php
@@ -67,7 +67,9 @@ AppAsset::register($this);
 
 <footer class="footer">
     <div class="container">
-        <p class="pull-left">&copy; Gobernación de Cundinamarca <?= date('Y') ?></p>
+        <p class="pull-left">&copy; My Company <?= date('Y') ?></p>
+
+        <p class="pull-right"><?= Yii::powered() ?></p>
     </div>
 </footer>
 
